724ed320944979f50a98e400c829a5d34cf394d1
Maferbravo1011 <mafermf1012@icloud.com>
2023-01-07 18:34:32 -0500

    proyecto2

---
+++ java.js
@@ -0,0 +1,6 @@
+function clicked (event) {
+  console.log('click desde la funcion')
+  alert('Ingresa al formulario de datos, clic aquí')
+  console.log(event)
+document.getElementById("Comprobantes").focus();
+}
---
+++ java2.js
@@ -0,0 +1,6 @@
+function agregar(){
+var comp = document.getElementById("comprobantes").value;
+var per = document.getElementById("periodos").value;
+var cant = document.getElementById("cantidad").value;
+console.log(comp);
+}
