b87ba27293fdd2ecfba2ba808d44a570d95c30e0
Maferbravo1011 <mafermf1012@icloud.com>
2023-01-07 20:32:23 -0500

    proyecto2act

--- java2.js
+++ java2.js
@@ -1,6 +1,19 @@
 function agregar(){
-var comp = document.getElementById("comprobantes").value;
-var per = document.getElementById("periodos").value;
-var cant = document.getElementById("cantidad").value;
-console.log(comp);
+let comp = document.getElementById("comprobantes").value;
+let per = document.getElementById("periodos").value;
+let cant = document.getElementById("cantidad").value;
+let cost = document.getElementById("costo").value;
+let lista = ["let comp","let per","let cant","let cost"]
+let [ c, b, a, d, e ] = lista
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+
+let inputValues = ({
+    "comprobantes": comp,
+    "periodos": per,
+    "cantidad": cant
+})
 }
